a81469d49d9b6010bc75d075d0470c6f66933b8b
hl-valdemar <valdemar.lorenzen@gmail.com>
2026-01-09 22:44:52 +0100

    wip: emit function with simple costant return value
    wip: emit function with simple costant return value
    
    wip: emit function with simple costant return value
    
    wip: emit function with simple costant return value

--- src/codegen/arm64.zig
+++ src/codegen/arm64.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const mem = @import("std").mem;
 
+pub const Register = enum(u4) { x0, x1, x2, x3, x4, x5, x6, x7 };
+
 /// ARM64 assembly emitter.
 pub const Arm64Emitter = struct {
     allocator: mem.Allocator,
@@ -25,42 +27,68 @@ pub const Arm64Emitter = struct {
 
     // DIRECTIVES
 
-    pub fn emitDirective(self: *Arm64Emitter, directive: []const u8) !void {
-        try self.buffer.appendSlice(self.allocator, directive);
+    pub fn directive(self: *Arm64Emitter, dir: []const u8) !void {
+        try self.buffer.appendSlice(self.allocator, dir);
         try self.buffer.append(self.allocator, '\n');
     }
 
-    pub fn emitLabel(self: *Arm64Emitter, name: []const u8) !void {
+    pub fn label(self: *Arm64Emitter, name: []const u8) !void {
         try self.buffer.appendSlice(self.allocator, name);
         try self.buffer.appendSlice(self.allocator, ":\n");
     }
 
-    pub fn emitGlobal(self: *Arm64Emitter, name: []const u8) !void {
+    pub fn global(self: *Arm64Emitter, name: []const u8) !void {
         try self.buffer.appendSlice(self.allocator, ".global ");
         try self.buffer.appendSlice(self.allocator, name);
         try self.buffer.append(self.allocator, '\n');
     }
 
-    pub fn emitComment(self: *Arm64Emitter, comment: []const u8, prefix: ?[]const u8) !void {
+    pub fn comment(self: *Arm64Emitter, msg: []const u8, prefix: ?[]const u8) !void {
         if (prefix) |p| try self.buffer.appendSlice(self.allocator, p);
         try self.buffer.appendSlice(self.allocator, "// ");
-        try self.buffer.appendSlice(self.allocator, comment);
+        try self.buffer.appendSlice(self.allocator, msg);
         try self.buffer.append(self.allocator, '\n');
     }
 
-    pub fn emitNewline(self: *Arm64Emitter) !void {
+    pub fn newline(self: *Arm64Emitter) !void {
         try self.buffer.append(self.allocator, '\n');
     }
 
     // INSTRUCTIONS
 
-    pub fn emitRet(self: *Arm64Emitter) !void {
+    pub fn ret(self: *Arm64Emitter) !void {
         try self.buffer.appendSlice(self.allocator, self.indent);
         try self.buffer.appendSlice(self.allocator, "ret\n");
     }
 
-    pub fn emitMovImmediate(self: *Arm64Emitter) !void {
-        _ = self;
-        // TODO: move immediate value to register (params)
+    // mov w<reg>, #<imm> (32-bit)
+    pub fn emitMovImm32(self: *Arm64Emitter, reg: Register, value: i32) !void {
+        var buf: [32]u8 = undefined;
+        const instr = std.fmt.bufPrint(&buf, "mov w{d}, #{d}\n", .{ reg, value }) catch unreachable;
+
+        try self.buffer.appendSlice(self.allocator, self.indent);
+        try self.buffer.appendSlice(self.allocator, instr);
+    }
+
+    // COMPOUND
+
+    pub fn funcPrologue(self: *Arm64Emitter) !void {
+        // c calling convention prologue:
+        // - save frame pointer and link register
+
+        // TODO: support more calling conventions
+
+        try self.buffer.appendSlice(self.allocator, self.indent);
+        try self.buffer.appendSlice(self.allocator, "str x29, x30, [sp, #-16]!\n");
+        try self.buffer.appendSlice(self.allocator, self.indent);
+        try self.buffer.appendSlice(self.allocator, "mov x29, sp\n");
+    }
+
+    pub fn funcEpilogue(self: *Arm64Emitter) !void {
+        // c calling convention epilogue:
+        // - restore frame pointer and link register
+
+        try self.buffer.appendSlice(self.allocator, self.indent);
+        try self.buffer.appendSlice(self.allocator, "ldp x29, x30, [sp], #16\n");
     }
 };
